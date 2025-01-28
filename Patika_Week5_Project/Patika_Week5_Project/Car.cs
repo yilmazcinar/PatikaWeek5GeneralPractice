5ea9ba0f4271161d71c137df5cb1da1a73411cf6
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-28 17:59:41 +0300

    Add files via upload

---
+++ Patika_Week5_Project/Patika_Week5_Project/Car.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patika_Week5_Project
+{
+    public class Car
+    {
+        public DateTime Time = new DateTime();
+        public string SerialNumber { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+        public int DoorNumber { get; set; }
+
+
+    }
+}
---
+++ Patika_Week5_Project/Patika_Week5_Project/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Reflection.Emit;
+using Patika_Week5_Project;
+
+
+
+
+List<Car> cars = new List<Car>();
+
+label:
+
+Console.WriteLine("Araba üretmek istiyor musunuz? (e/h)");
+
+string userInput = Console.ReadLine().ToLower().Trim();
+while (userInput != "e" && userInput != "h")
+{
+    Console.WriteLine("Hatalı giriş E ya da H giriniz");
+    userInput = Console.ReadLine().ToLower().Trim();
+
+
+}
+
+if (userInput == "e")
+{
+    Car car = new Car();
+    Console.Write("Seri numarası: ");
+    car.SerialNumber = Console.ReadLine();
+    Console.Write("Marka: ");
+    car.Brand = Console.ReadLine();
+    Console.Write("Model: ");
+    car.Model = Console.ReadLine();
+    Console.Write("Renk: ");
+    car.Color = Console.ReadLine();
+start:
+    try
+    {
+        Console.Write("Kapı sayısı: ");
+        car.DoorNumber = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception)
+    {
+
+        Console.WriteLine("Geçersiz değer");
+        goto start;
+    }
+
+    cars.Add(car);
+    goto label;
+
+}
+else
+{
+    if (cars.Count != 0)
+    {
+        foreach (Car car in cars)
+        {
+            Console.WriteLine($"Seri Numarası: {car.SerialNumber} - Marka: {car.Brand}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Listelencek araba yoktur");
+
+    }
+
+}
+
+
+ 
